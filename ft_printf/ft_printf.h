576e383782859541a61059c3eaa75f729cf69f25
Ruben Teles <75394884+RubenTeles@users.noreply.github.com>
2022-01-09 01:03:08 +0000

    100%
    [100%] • %c print a single character.
    [100%] • %s print a string of characters.
    [100%] • %p The void * pointer argument is printed in hexadecimal.
    [100%]• %d print a decimal (base 10) number.
    [100%]• %i print an integer in base 10.
    [100%]• %u print an unsigned decimal (base 10) number.
    [100%] • %x print a number in hexadecimal (base 16), with lowercase.
    [100%] • %X print a number in hexadecimal (base 16), with uppercase.
    [100%] • %% print a percent sign.*/
    [100%] • mix
    
    Norminette - Pass!

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 16:28:00 by rteles            #+#    #+#             */
-/*   Updated: 2022/01/09 00:49:48 by rteles           ###   ########.fr       */
+/*   Updated: 2022/01/09 00:58:17 by rteles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,5 @@ int	ft_print_int(int n);
 int	ft_print_unsigned_decimal(unsigned int un);
 int	ft_print_hx(unsigned long long hx, char *base);
 int	ft_print_pointer(unsigned long long pointer);
-
 
 #endif
